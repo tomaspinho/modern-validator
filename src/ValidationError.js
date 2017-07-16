d4dade9987a5030ad380de3cf18c3670b2c3935b
Tomás Pinho <me@tomaspinho.com>
2017-07-16 11:49:47 +0100

    add ValidationError class prototype

---
+++ src/ValidationError.js
@@ -0,0 +1,35 @@
+class ValidationError extends Error {
+
+    constructor(validation) {
+        super(ValidationError.message);
+        this.validation = validation;
+        // We don't want to capture the Stack, no useful information there
+        this.stack = "";
+    }
+
+    // Pretty print the error
+    inspect(depth, opts) {
+
+        let validationString = JSON.stringify(this.validation, null, ' ')
+            // removes quotes from prop names
+            .replace(/\"([^(\")"]+)\":/g,"$1:");
+
+        // ident properly for final string
+        const [first, ...rest] = validationString.split('\n');
+        const formattedRest = rest.map(s => `  ${s}`);
+        validationString = [first, ...formattedRest].join('\n');
+
+        return `{ ${this.toString()}\n  validation: ${validationString}\n}`;
+    }
+
+    toString() {
+        return `${this.constructor.name}: ${ValidationError.message}`;
+    };
+
+    // We don't want to capture the Stack, no useful information there
+    captureStackTrace () {}
+}
+
+ValidationError.message = "Modern Validator encountered validation error(s).";
+
+module.exports = ValidationError;
